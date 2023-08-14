6b9fe2bfa180be618b64fd56bc639697bdb74836
Xero kun <elo48858@gmail.com>
2023-08-14 15:20:15 +0100

    `TEST` Tested min and max priority queues

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,25 @@ import datastructures.priorityqueues.MinPriorityQueue
 fun main() {
     val maxPriorityQueue = MaxPriorityQueue<Int>()
     val minPriorityQueue = MinPriorityQueue<Int>()
+
+    maxPriorityQueue.insertItem(1)
+    maxPriorityQueue.insertItem(2)
+    maxPriorityQueue.insertItem(7)
+    maxPriorityQueue.insertItem(3)
+    maxPriorityQueue.insertItem(6)
+
+    println("Max Item: ${maxPriorityQueue.max()}")
+    maxPriorityQueue.removeMax()
+    println("New Size: ${maxPriorityQueue.size}")
+
+    minPriorityQueue.insertItem(8)
+    minPriorityQueue.insertItem(3)
+    minPriorityQueue.insertItem(2)
+    minPriorityQueue.insertItem(6)
+    minPriorityQueue.insertItem(1)
+
+    println("\nMin Item: ${minPriorityQueue.removeMin()}")
+    println("New Min Item: ${minPriorityQueue.removeMin()}")
+    println("New Size ${minPriorityQueue.size}")
+    println("New Min: ${minPriorityQueue.min()}")
 }
